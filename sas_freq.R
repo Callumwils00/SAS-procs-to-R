7a15243fe6ebb2a2a62faea87aadd323f5a3a4e2
Callum Wilson <callum.wilson@cso.ie>
2024-03-07 13:13:32 +0000

    Fixed bug with distinct in the internal proc function added nested freq tabling functionality.

--- sas_freq.R
+++ sas_freq.R
@@ -1,28 +1,21 @@
-if(!require(tidyverse)){library(tidyverse, quietly = TRUE)}
-if(!require(dplyr)){library(dplyr, quietly = TRUE)}
-if(!require(rlang)){library(rlang, quietLy = TRUE)}
-if(!require(gt)){library(gt, quietly = TRUE)}
 
-
-sas_freq <- function(data , by_vars, table = FALSE, as_list = "AS_TABLES"){
+sas_freq <- function(data ,..., by_vars = NA, table = FALSE, as_list = "AS_TABLES"){
   if(is.data.frame(data) == FALSE){
     stop("Data must be a dataframe")
-  }else if(table %ni% c(TRUE, FALSE)){
+  }else if(!table %in% c(TRUE, FALSE)){
     stop("table argument must be TRUE or FALSE")
-  }else if(as_list %ni% c("AS_TABLES", "AS_TAB", "AS_LIST")){
+  }else if(!as_list %in% c("AS_TABLES", "AS_TAB", "AS_LIST")){
     stop("Invalid as_list option")
   }
   
-  byvars <- rlang::parse_exprs(by_vars)
-  
-  proc <- function(var){
-    df <- data %>% dplyr::arrange(!!!var) %>%
-      group_by(!!!var) %>% 
+  proc <- function(...){
+    df <- data %>% dplyr::arrange(...) %>%
+      group_by(...) %>% 
       dplyr::mutate(num = n()) %>% ungroup() %>%
+      distinct(..., .keep_all = TRUE) %>%
       mutate(perc = num/sum(num),
              cumulative_perc = cumsum(num)/sum(num)) %>%
-      distinct(!!!var, .keep_all = TRUE) %>%
-      dplyr::select(!!!var, num, perc, cumulative_perc)
+      dplyr::select(..., num, perc, cumulative_perc)
     if(table == TRUE){
       df <- df %>% gt
     }
@@ -31,16 +24,29 @@ sas_freq <- function(data , by_vars, table = FALSE, as_list = "AS_TABLES"){
   
   if(as_list == "AS_TABLES"){
     if(table == FALSE){
-      out <- map(byvars, proc)}
-    else{
-      out <- map(map(byvars, proc), gt)
+      if(is.na(by_vars) == FALSE){
+        byvars <- rlang::parse_exprs(by_vars)
+        out <- map(byvars, proc)
+      } else{
+        out <- proc(...)
+      }
     }
-  } else if(as_list == "AS_TAB"){
-    temp <- proc(byvars) %>% select(!!!byvars, perc)
-    out <- temp %>% pivot_wider(names_from = (!!!eval(byvars[1])), values_from = perc,
-                                values_fill = 0)
-  } else{
-    out <- proc(byvars)
+    }
+    else{
+      if(is.na(by_vars) == FALSE){
+        out <- map(map(byvars, proc),gt)
+      } else{
+        out <- gt(proc(...))
+      }
+    }
+    #else if(as_list == "AS_TAB"){
+      #temp <- proc(byvars) %>% select(!!!byvars, perc)
+      #out <- temp %>% pivot_wider(names_from = (!!!eval(byvars[1])), values_from = perc,
+      #        values_fill = 0)
+    #} else{
+      #out <- proc(byvars)
+    #}
+    return(out)
   }
-  return(out)
-}
+
+sas_freq(mtcars, by_vars = "cyl;carb")
